9da97c2020dac44b2b07adeee05d3a10bca8a8e5
Poyo <poyoarya@gmail.com>
2017-12-11 12:50:40 -0800

    Basic text processing

---
+++ Assets/Scripts/ConsoleInput.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConsoleInput : MonoBehaviour {
+	public InputField InputField;
+	public Text OutputText;
+
+	public void Start() {
+		InputField.onEndEdit.AddListener(x => Process(x));
+	}
+
+	private Regex removerizer = new Regex(@"[^A-Z0-9]");
+	private Regex splitter = new Regex(@"\s+");
+
+	public void Process(string input) {
+		Output("> " + input);
+		InputField.text = String.Empty;
+		
+		string[] processed = splitter
+			.Split(input)
+			.Select(s => removerizer.Replace(s.ToUpperInvariant(), String.Empty))
+			.ToArray();
+
+		Output(String.Join(", ", processed));
+	}
+
+	public void Output(string output) {
+		OutputText.text += "\n" + output;
+	}
+}
---
+++ Assets/Scripts/ConsoleInputField.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ConsoleInputField : InputField {
+	public override void OnDeselect(BaseEventData eventData) {
+		// TODO: nothing works
+	}
+}
