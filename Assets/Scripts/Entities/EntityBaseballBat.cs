54686763b93c1edd0eb1da9c9b4f8cfb598b6fd9
Poyo <poyo_ssb@hotmail.com>
2018-01-11 14:23:15 -0800

    added possession tests

---
+++ Assets/Scripts/Entities/EntityBaseballBat.cs
@@ -0,0 +1,11 @@
+﻿public class EntityBaseballBat : EntityObject {
+	public override void Initialize() {
+		base.Initialize();
+		this.Cases = new Cases {
+			NominativeSingular = "baseball bat",
+			GenitiveSingular = "baseball bat's",
+			NominativePlural = "baseball bats",
+			GenitivePlural = "baseball bats'"
+		};
+	}
+}
--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -16,10 +16,17 @@ public class TestScript : MonoBehaviour {
 
 		Player.Human = player;
 
-		EntityDoor door = World.AddEntity<EntityDoor>(null, Location.Get("room"));
-		EntityBox box = World.AddEntity<EntityBox>(null, Location.Get("room"));
-		EntityKey key = World.AddEntity<EntityKey>(null, Location.Get("room"));
-		EntityEgg egg = World.AddEntity<EntityEgg>(null, Location.Get("room"));
+		EntityBaby baby = World.AddEntity<EntityBaby>("room");
+		EntityEgg babyEgg = World.AddEntity<EntityEgg>("room");
+		EntityBaseballBat babyBaseballBat = World.AddEntity<EntityBaseballBat>("room");
+
+		baby.AddPossession(babyEgg);
+		baby.AddPossession(babyBaseballBat);
+
+		EntityDoor door = World.AddEntity<EntityDoor>("room");
+		EntityBox box = World.AddEntity<EntityBox>("room");
+		EntityKey key = World.AddEntity<EntityKey>("room");
+		EntityEgg egg = World.AddEntity<EntityEgg>("room");
 
 		door.Open = false;
 		door.Locked = true;
@@ -30,13 +37,14 @@ public class TestScript : MonoBehaviour {
 		key.In = box;
 		egg.In = box;
 
-		// TODO: Remove console
+		// TODO: Remove Console
 		Player.Parse("open the chest", this.Console);
-		Player.Parse("take the key from the chest", this.Console);
-		Player.Parse("take the egg", this.Console); // implicit egg in chest
+		Player.Parse("take the egg from the chest", this.Console);
+		Player.Parse("take the baby's egg", this.Console);
+		Player.Parse("take the key", this.Console);
 		Player.Parse("unlock the door with the key", this.Console);
 		Player.Parse("open the door", this.Console);
-		Player.Parse("eat the egg", this.Console);
-		Player.Parse("kick the knee", this.Console);
+		Player.Parse("eat the eggs", this.Console);
+		Player.Parse("take the baby's baseball bat", this.Console);
 	}
 }
